ffa9b96543461426fe745233a43440ec91111adb
SalemMarye <114462896+SalemMarye@users.noreply.github.com>
2023-03-23 16:58:36 -0400

    Add files via upload

---
+++ Interacive TIp Calculator.py
@@ -0,0 +1,6 @@
+food_amount = float(input('Enter your food amount : $'))
+tip_percentage = float(input('Enter your tip amount: $'))
+
+tip_amount = food_amount * tip_percentage
+
+print('$' + str(tip_amount))
